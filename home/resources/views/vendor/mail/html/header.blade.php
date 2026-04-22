b6f2a5111da4f6553640bf1dd3f8a1a95e8cf6c9
Luis Godino Wallace <lgodwal2312@g.educaand.es>
2026-04-22 12:29:15 +0200

    incrustar logo en correo de verificacion

--- home/resources/views/vendor/mail/html/header.blade.php
+++ home/resources/views/vendor/mail/html/header.blade.php
@@ -2,10 +2,24 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'OutfitGo' || trim($slot) === 'Laravel')
-<img src="{{ config('app.url') }}/img/logo.png" class="logo" alt="{{ config('app.name') }}" style="max-height: 50px;">
+@php
+    $logoPath = public_path('img/logo.png');
+    $logoSrc = null;
+    if (file_exists($logoPath)) {
+        try {
+            $logoSrc = $message->embed($logoPath);
+        } catch (\Exception $e) {
+            $logoSrc = config('app.url') . '/img/logo.png';
+        }
+    }
+@endphp
+
+@if ($logoSrc)
+    <img src="{{ $logoSrc }}" class="logo" alt="{{ config('app.name') }}" style="max-height: 50px;">
+@elseif (trim($slot) === 'OutfitGo' || trim($slot) === 'Laravel')
+    <img src="{{ config('app.url') }}/img/logo.png" class="logo" alt="{{ config('app.name') }}" style="max-height: 50px;">
 @else
-{!! $slot !!}
+    {!! $slot !!}
 @endif
 </a>
 </td>
